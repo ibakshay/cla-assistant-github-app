36b3d085a3addede332b293352dca083c1241515
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-07-28 23:23:49 +0200

    Initial Commit 2

--- index.js
+++ index.js
@@ -2,13 +2,41 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
+
+
+
+
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
   app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+    const issueComment = context.issue({
+      body: 'Thanks for opening this issue!'
+    })
+    console.log(context.payload)
+    try {
+      const response = await context.github.issues.createComment(issueComment)
+      console.log(response)
+    } catch (e) {
+      console.log(e)
+    }
+
+  })
+
+  app.on('pull_request.opened', async context => {
+    const issueComment = context.issue({
+      body: 'Thanks for opening the pull request!'
+    })
+    const pullNumber = context.payload.pull_request.number;
+    const repo = context.payload.pull_request.head.repo
+    const repoOwner = repo.owner.login
+    const repoName = repo.name
+    try {
+      const response = await context.github.issues.createComment(issueComment)
+    } catch (e) {
+      console.log(e)
+    }
   })
 
   // For more information on building apps:
@@ -17,3 +45,11 @@ module.exports = app => {
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
 }
+
+
+// const commits = await context.github.pullRequests.listCommits({
+//   owner: repoOwner,
+//   repo: repoName,
+//   number: pullNumber
+// });
+// console.log(commits)
